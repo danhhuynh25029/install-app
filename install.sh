36c2c56695790143e808f507ec6778f07397b96b
danhhuynh25029 <danhhuynh250@gmail.com>
2021-09-02 13:10:40 +0700

    update file

--- install.sh
+++ install.sh
@@ -1,3 +1,21 @@
 sudo apt install vim
-test
+# install golang
+GOVERSION="go1.12.9"
+
+apt update
+apt -y upgrade
+
+echo https://storage.googleapis.com/golang/$GOVERSION.linux-amd64.tar.gz
+curl -O https://storage.googleapis.com/golang/$GOVERSION.linux-amd64.tar.gz
+tar -xvf $GOVERSION.linux-amd64.tar.gz
+rm -rf /usr/local/go
+mv go /usr/local
+rm $GOVERSION.linux-amd64.tar.gz
+
+
+echo 'export PATH=$PATH:/usr/local/go/bin' >>~/.profile
+echo 'export GOPATH=$HOME/go' >>~/.profile 
+echo 'export PATH=$PATH:$GOPATH/bin' >>~/.profile
+
+source ~/.profile
 
